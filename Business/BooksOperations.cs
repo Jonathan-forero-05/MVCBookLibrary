ced56d5edc0c07849624aa5b3d25880ab32b1ff3
Jonathan Forero <jonathanforero05@gmail.com>
2019-10-23 18:04:01 -0500

    initial commit

---
+++ Business/BooksOperations.cs
@@ -0,0 +1,39 @@
+﻿using Contracts;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DTOs;
+using Data;
+using System;
+
+namespace Business
+{
+    public class BooksOperations : IBooksOperations
+    {
+        private readonly IBookRepository _bookRepository;
+
+        public BooksOperations(IBookRepository genericRepository)
+        {
+            _bookRepository = genericRepository;
+        }
+
+        public async Task<int> DeleteBook(int Id)
+        {
+            return await _bookRepository.Delete(x => x.ID == Id);
+        }
+
+        public async Task<IList<Book>> GetAllBooks()
+        {
+            return await _bookRepository.GetAll();
+        }
+
+        public async Task<IList<Book>> GetBooksByAuthor(string author)
+        {
+            return await _bookRepository.GetBy(b => b.Author == author);
+        }
+
+        public async Task<IList<Book>> GetBooksByCategory(string category)
+        {
+            return await _bookRepository.GetBy(b => b.Category == category);
+        }
+    }
+}
---
+++ Data/Base/GenericRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Data.Base
+{
+    public abstract class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly LibraryContext context;
+
+        public GenericRepository()
+        {
+            context = new LibraryContext();
+        }
+
+        public async Task<int> Delete(Expression<Func<T, bool>> func)
+        {
+            var itemsToRemove = await context.Set<T>().Where(func).ToListAsync();
+
+            foreach (var item in itemsToRemove)
+            {
+                context.Set<T>().Remove(item);
+            }
+
+            return await context.SaveChangesAsync();
+
+        }
+
+        public async Task<IList<T>> GetAll()
+        {
+            return await context.Set<T>().ToListAsync();
+        }
+
+        public async Task<IList<T>> GetBy(Expression<Func<T, bool>> func)
+        {
+            return await context.Set<T>().Where(func).ToListAsync();
+        }
+
+        public async Task<T> Insert(T entity)
+        {
+            context.Set<T>().Add(entity);
+            await context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> Update(Expression<Func<T, bool>> func, T entity)
+        {
+            T existing = await context.Set<T>().Where(func).FirstOrDefaultAsync();
+
+            context.Entry(existing).CurrentValues.SetValues(entity);
+            await context.SaveChangesAsync();
+
+            return existing;
+        }
+    }
+}
